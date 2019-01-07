9aefefa177951991e505b5e6acc9a956082c77fb
anton <angeloffree@yandex.ru>
2019-01-07 18:24:34 +0300

    Добавил иконки

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -4,11 +4,53 @@
       <h1>Немного о нас</h1>
       <h2>Привет, мы MoonSolution - команда разработчиков-энтузиастов в духе rock`n`roll. </h2>
       <h2>Мы здесь, чтобы решить вашу проблему. </h2>
-      <br><br><br><br><br><br><br><br><br><br><br>
+      <div class="wrapper">
+        <div>
+          <h2>Персонализация</h2>
+          <div class="icon icon-heart"></div>
+          <p>И вот, почему мы делаем это с любовью и всем таким - ...</p>
+        </div>
+        <div>
+          <h2>Персонализация</h2>
+          <div class="icon icon-globe"></div>
+          <p>И вот, почему мы делаем это с любовью и всем таким - ...</p>
+        </div>
+        <div>
+          <h2>Персонализация</h2>
+          <div class="icon icon-ok"></div>
+          <p>И вот, почему мы делаем это с любовью и всем таким - ...</p>
+        </div>
+        <div>
+          <h2>Персонализация</h2>
+          <div class="icon icon-home"></div>
+          <p>И вот, почему мы делаем это с любовью и всем таким - ...</p>
+        </div>
+      </div>
     </section>
     <section class="dark kind">
       <h1>Вот, что мы можем</h1>
-      <br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <div class="wrapper">
+        <div>
+          <h2>Персонализация</h2>
+          <div class="icon icon-heart"></div>
+          <p>И вот, почему мы делаем это с любовью и всем таким - ...</p>
+        </div>
+        <div>
+          <h2>Персонализация</h2>
+          <div class="icon icon-globe"></div>
+          <p>И вот, почему мы делаем это с любовью и всем таким - ...</p>
+        </div>
+        <div>
+          <h2>Персонализация</h2>
+          <div class="icon icon-ok"></div>
+          <p>И вот, почему мы делаем это с любовью и всем таким - ...</p>
+        </div>
+        <div>
+          <h2>Персонализация</h2>
+          <div class="icon icon-home"></div>
+          <p>И вот, почему мы делаем это с любовью и всем таким - ...</p>
+        </div>
+      </div>
     </section>
 
     <section class="light">
@@ -16,7 +58,28 @@
       <h1>Немного о нас</h1>
       <h2>Привет, мы MoonSolution - команда разработчиков-энтузиастов в духе rock`n`roll. </h2>
       <h2>Мы здесь, чтобы решить вашу проблему. </h2>
-      <br><br><br><br><br><br><br><br><br>
+            <div class="wrapper">
+        <div>
+          <h2>Персонализация</h2>
+          <div class="icon icon-heart"></div>
+          <p>И вот, почему мы делаем это с любовью и всем таким - ...</p>
+        </div>
+        <div>
+          <h2>Персонализация</h2>
+          <div class="icon icon-globe"></div>
+          <p>И вот, почему мы делаем это с любовью и всем таким - ...</p>
+        </div>
+        <div>
+          <h2>Персонализация</h2>
+          <div class="icon icon-ok"></div>
+          <p>И вот, почему мы делаем это с любовью и всем таким - ...</p>
+        </div>
+        <div>
+          <h2>Персонализация</h2>
+          <div class="icon icon-home"></div>
+          <p>И вот, почему мы делаем это с любовью и всем таким - ...</p>
+        </div>
+      </div>
     </section>
   </div>
 </template>
@@ -51,6 +114,26 @@ section {
   &.dark {
     background-color: #1f1f1f;
     color: #F8F8F8;
+  }
+
+  .wrapper {
+    display: flex;
+    margin: 80px 0 20px 0;
+
+    div {
+      margin: 0 20px;
+      text-align: center;
+
+      h2 {
+        font-size: 1.3rem;
+        text-transform: uppercase;
+      }
+
+      .icon {
+        font-size: 7rem;
+      }
+
+    }
   }
 }
 </style>
